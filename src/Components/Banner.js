a71424f6831abbd8b547323ede7cd44ed5941a60
unknown <leoinhosagaz@hotmail.com>
2019-04-13 21:17:38 -0300

    Ajeitando o component Banner para Mobile

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,27 +3,36 @@ import '../CSS/Banner.css';
 import Logo from '../Img/Logo.png';
 import Lupa from '../Img/Lupa.svg';
 import Cesta from '../Img/Cesta.svg';
+import Menu from '../Img/Menu.png';
 
 class Banner extends Component {
   render() {
     return (
       <div className="banner">
-        <nav>
-            <img src={Logo}/>
+        <nav>   
+            <img className="menuMobi" src={Menu}/>
+            <img className="logoBanner" src={Logo}/>
             <ul>
-                <li>Diferenciais</li>
-                <li>Produtos</li>
-                <li><img src={Lupa}/></li>
-                <li><div className="cesta"><img src={Cesta}/><div className="contador"><span>2</span></div></div></li>
+                <li className="visivel">Diferenciais</li>
+                <li className="visivel">Produtos</li>
+                <li className="visivel"><img src={Lupa}/></li>
+                <li className="visivel2"><div className="cesta"><img src={Cesta}/><div className="contador"><span>2</span></div></div></li>
                               
             </ul>
         </nav>
         <div className="textoPrincipal">
             <h1>Omnichannel</h1>
-            <p>
+            <p className="textoDesktop">
                 Nossa loja conceito reune as principais<br/>
                 funcionalidades Vtex, para revolucionar<br/>
-                a forma dos seus clientes interagirem dp<br/>
+                a forma dos seus clientes interagirem do<br/>
+                e-commerrce.
+            </p>
+            <p className="textoMobi">
+                Nossa loja conceito reune as<br/>
+                principais funcionalidades Vtex,<br/>
+                para revolucionar a forma dos<br/>
+                seus clientes interagirem do<br/>
                 e-commerrce.
             </p>
             <div className="saibaMais">
